d2da134664fff99c71ad37e62b710bb8fe62029e
Raed Altawil <raed.altawil19@gmail.com>
2026-08-02 17:15:01 +0000

    File changes

--- src/pages/Builder.jsx
+++ src/pages/Builder.jsx
@@ -264,7 +264,7 @@ export default function Builder() {
           </div>
         )}
         {!processing && (
-          <div ref={panelRef} className="flex-1 overflow-y-auto overflow-x-hidden p-6 bg-slate-200/60 print:p-0 print:bg-white print:overflow-visible flex">
+          <div ref={panelRef} className="cv-builder-panel flex-1 overflow-y-auto overflow-x-hidden p-6 bg-slate-200/60 print:p-0 print:bg-white print:overflow-visible print:block flex">
             <div className="no-print fixed top-1/2 right-4 -translate-y-1/2 z-30 print:hidden">
               <CVSideToolbar
                 mode={mode}
@@ -286,7 +286,7 @@ export default function Builder() {
                 onZoomFit={zoomFit}
               />
             </div>
-            <div style={{ width: A4_W * scale, height: contentH * scale }} className="m-auto print:w-auto print:h-auto">
+            <div style={{ width: A4_W * scale, height: contentH * scale }} className="cv-scale-parent m-auto print:w-auto print:h-auto">
               <div ref={wrapperRef} className="cv-scale-wrapper relative" style={{ transform: `scale(${scale})`, transformOrigin: "top left", width: A4_W }}>
                 <CVPages templateId={templateId} data={data} editable={mode === "edit"} actions={actions} layout={layout} mode={mode} />
                 {mode === "edit" && (
